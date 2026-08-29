96a524aa913aa8f007e2da1585a2edc2755d874a
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-08-29 19:44:25 +0530

    done with DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log("hello sharu");
 alert("hey  idiot!");
 console.log(window);
-window.alert("hey who are you?")
+window.alert("hey who are you?");
+let heading = document.getElementById("good");
+console.log(heading);
+let head2 = document.getElementsByClassName("bad");
+console.log(head2);
